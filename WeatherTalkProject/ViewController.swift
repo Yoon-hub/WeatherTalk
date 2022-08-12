5ebfd19308451f8e15831d51aa1b27c04321a13c
Yoon-hub <kong4170@naver.com>
2022-08-12 23:55:16 +0900

    시작 해봅시다

---
+++ WeatherTalkProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherTalkProject
+//
+//  Created by 최윤제 on 2022/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
